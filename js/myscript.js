25abd830f608037fa8000940f8c48036583eb8e2
NADERALHENDI <naderalhendi41@gmail.com>
2020-09-22 22:53:29 +0300

    fix js.

--- js/myscript.js
+++ js/myscript.js
@@ -2,19 +2,20 @@ var fname;
 var lname;
 var age;
 var goto;
-fname = prompt ('WELCOME TO YOUR POCKET LIPRARY inter your first name, please')
-lname = prompt (fname+' inter your last name, please')
-age = prompt (fname+' inter your age, please')
+fname = prompt ('WELCOME TO YOUR POCKET LIPRARY enter your first name, please')
+lname = prompt (fname+' enter your last name, please')
+age = prompt (fname+' enter your age, please')
 if (age > 0 ) {
     if (age < 8 ){
-        goto = '....go to pecture books list'
+        goto = 'Our advice to you: go to pecture books list'
     } else if(age < 12 ){
-        goto = '....go to middle-grade books list'
+        goto = 'Our advice to you: go to middle-grade books list'
     } else if(age < 18 ) {
-        goto = '....go to young adult books list'
+        goto = 'Our advice to you: go to young adult books list'
     } else {
         goto = '....read what you like'
     }
 } else { goto = '....it is a WRONG AGE !!!'}
 var total = fname+' '+lname+'  '+goto
+alert (goto);
 document.write('<h3><em>'+total+'</em></h3>');
