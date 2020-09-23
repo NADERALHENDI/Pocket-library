2d1524d9828d3b7465fa9b1d96bbd2a35a83ab19
NADERALHENDI <naderalhendi41@gmail.com>
2020-09-23 12:47:17 +0300

    convert to function

--- js/myscript.js
+++ js/myscript.js
@@ -2,9 +2,11 @@ var fname;
 var lname;
 var age;
 var goto;
+
 fname = prompt ('WELCOME TO YOUR POCKET LIPRARY enter your first name, please')
 lname = prompt (fname+' enter your last name, please')
 age = prompt (fname+' enter your age, please')
+function total(age) {
 if (age > 0 ) {
     if (age < 8 ){
         goto = 'Our advice to you: go to pecture books list'
@@ -19,3 +21,5 @@ if (age > 0 ) {
 var total = fname+' '+lname+'  '+goto
 alert (goto);
 document.write('<h3><em>'+total+'</em></h3>');
+}
+total(age)
